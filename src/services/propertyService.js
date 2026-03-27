9f2b878299be80614607570667ff348e6d5e6718
LakshmiprabaS <lakshmipraba.S@cognizant.com>
2026-03-27 09:03:27 +0530

    change redux to service

--- src/services/propertyService.js
+++ src/services/propertyService.js
@@ -1,38 +1,56 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
 
-const API_BASE_URL = "http://localhost:5154/api/Property";
+const API_BASE_URL =
+  process.env.REACT_APP_API_BASE_URL
+    ? `${process.env.REACT_APP_API_BASE_URL}/api/Property`
+    : 'http://localhost:5154/api/Property';
 
 const getHeaders = () => ({
   headers: {
     Authorization: `Bearer ${Cookies.get('accessToken')}`,
-  }
+    'Content-Type': 'application/json',
+  },
 });
+
 
 export const propertyService = {
   getProperties: async (params) => {
-    return await axios.get(API_BASE_URL, { params, ...getHeaders() });
+    const response = await axios.get(API_BASE_URL, {
+      params,
+      ...getHeaders(),
+    });
+    return response.data;
   },
 
   getPropertyById: async (id) => {
-    return await axios.get(`${API_BASE_URL}/${id}`, getHeaders());
+    const response = await axios.get(`${API_BASE_URL}/${id}`, getHeaders());
+    return response.data;
   },
 
   createProperty: async (propertyData) => {
-    // Standard JSON POST
-    const response = await axios.post(API_BASE_URL, propertyData, getHeaders());
-    return response.data; 
+    const response = await axios.post(
+      API_BASE_URL,
+      propertyData,
+      getHeaders()
+    );
+    return response.data;
   },
 
   updateProperty: async (id, propertyData) => {
-    // PUT request to update existing property
-    const response = await axios.put(`${API_BASE_URL}/${id}`, propertyData, getHeaders());
+    const response = await axios.put(
+      `${API_BASE_URL}/${id}`,
+      propertyData,
+      getHeaders()
+    );
     return response.data;
   },
 
   deleteProperty: async (id) => {
-    const response = await axios.delete(`${API_BASE_URL}/${id}`, getHeaders());
+    const response = await axios.delete(
+      `${API_BASE_URL}/${id}`,
+      getHeaders()
+    );
     return response.data;
   },
-
 };
